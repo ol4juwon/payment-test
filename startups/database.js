3f52959736c9dec190ee7fbc6930f52cba32f004
Ola Juwon <ol4juwon>
2025-01-25 02:10:45 +0100

    cleanup

--- startups/database.js
+++ startups/database.js
@@ -4,7 +4,7 @@ const mongoose =  require("mongoose");
 
 mongoose.set('debug', true);
 const monogodbUrl = `${process.env.MONGODB_URL}?retryWrites=true`;
-// console.log("MONGO_DB", monogodbUrl);
+
 mongoose.connect(monogodbUrl,{useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology:  true});
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
